f89bc6cdb6390346d68005d76a87de4002e36d66
Yuichi Yabu <yuichi.yabu@ripplex.com>
2016-05-25 13:01:18 +0900

    when構文を試した

--- app/src/main/java/com/tumpaca/tumpaca/MainActivity.kt
+++ app/src/main/java/com/tumpaca/tumpaca/MainActivity.kt
@@ -2,11 +2,29 @@ package com.tumpaca.tumpaca
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val i = 2
+
+        when (i) {
+            0 -> setText("i is 0")
+            in 1..2 -> setText("i is 1 or 2")
+            3, 4 -> setText("i is 3 or 4")
+            5 or 6 -> setText("i is 5 or 6")
+            else -> setText("else")
+        }
+
+    }
+
+    fun setText(message: String) {
+        val textview = this.findViewById(R.id.Message) as TextView?
+        textview?.setText(message)
     }
 }
