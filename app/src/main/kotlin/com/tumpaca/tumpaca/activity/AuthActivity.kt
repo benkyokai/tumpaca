c2f9cbbf8aae5e27416c61d27146377120b56692
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-06-03 15:45:13 +0900

    Tumblr APIキーをlocal.propertiesから読み込んでAPKに組み込む

--- app/src/main/kotlin/com/tumpaca/tumpaca/activity/AuthActivity.kt
+++ app/src/main/kotlin/com/tumpaca/tumpaca/activity/AuthActivity.kt
@@ -3,19 +3,21 @@ package com.tumpaca.tumpaca.activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Base64
 import android.util.Log
 import android.widget.Button
 import com.tumblr.loglr.LoginResult
 import com.tumblr.loglr.Loglr
 import com.tumpaca.tumpaca.R
+import java.util.*
 
 /**
  * Created by amake on 6/1/16.
  */
 class AuthActivity: AppCompatActivity() {
 
-    var consumerKey = ""
-    var consumerSecret = ""
+    var consumerKey: String? = null
+    var consumerSecret: String? = null
     var urlCallback = "tumpaca://tumblr/auth/ok"
 
     var tag = "AuthActivity"
@@ -28,6 +30,11 @@ class AuthActivity: AppCompatActivity() {
 
         var authButton = findViewById(R.id.button_authorize) as Button
         authButton.setOnClickListener { doAuth() }
+
+        var authProps = Properties()
+        resources.openRawResource(R.raw.auth).use { authProps.load(it) }
+        consumerKey = String(Base64.decode(authProps.get("tumblr.consumer.key") as String, Base64.DEFAULT))
+        consumerSecret = String(Base64.decode(authProps.get("tumblr.consumer.secret") as String, Base64.DEFAULT))
     }
 
     fun doAuth() {
@@ -41,7 +48,6 @@ class AuthActivity: AppCompatActivity() {
     }
 
     fun onLogin(r: LoginResult) {
-        Log.v(tag, "Got login result: ${r.oAuthToken}, ${r.oAuthTokenSecret}")
         var intent = Intent(this, DashboardActivity::class.java)
         intent.putExtra("consumerKey", consumerKey)
         intent.putExtra("consumerSecret", consumerSecret)
