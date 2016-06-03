ffeb743fc7f395dc2ca4dd011307df9ce9eb49b5
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-06-03 16:35:07 +0900

    OAuthのtokenとsecretを永続化

--- app/src/main/kotlin/com/tumpaca/tumpaca/activity/AuthActivity.kt
+++ app/src/main/kotlin/com/tumpaca/tumpaca/activity/AuthActivity.kt
@@ -16,9 +16,17 @@ import java.util.*
  */
 class AuthActivity: AppCompatActivity() {
 
+    final var credentialsFile = "credentials.properties"
+    final var urlCallback = "tumpaca://tumblr/auth/ok"
+    final var consumerKeyProp = "tumblr.consumer.key"
+    final var consumerSecretProp = "tumblr.consumer.secret"
+    final var authTokenProp = "auth.token"
+    final var authTokenSecretProp = "auth.token.secret"
+
     var consumerKey: String? = null
     var consumerSecret: String? = null
-    var urlCallback = "tumpaca://tumblr/auth/ok"
+    var authToken: String? = null
+    var authTokenSecret: String? = null
 
     var tag = "AuthActivity"
 
@@ -26,15 +34,26 @@ class AuthActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        loadCredentials()
+
         setContentView(R.layout.activity_auth)
 
         var authButton = findViewById(R.id.button_authorize) as Button
         authButton.setOnClickListener { doAuth() }
+    }
 
+    fun loadCredentials() {
         var authProps = Properties()
         resources.openRawResource(R.raw.auth).use { authProps.load(it) }
-        consumerKey = String(Base64.decode(authProps.get("tumblr.consumer.key") as String, Base64.DEFAULT))
-        consumerSecret = String(Base64.decode(authProps.get("tumblr.consumer.secret") as String, Base64.DEFAULT))
+        consumerKey = String(Base64.decode(authProps.get(consumerKeyProp) as String, Base64.DEFAULT))
+        consumerSecret = String(Base64.decode(authProps.get(consumerSecretProp) as String, Base64.DEFAULT))
+
+        assert(consumerKey != null)
+        assert(consumerSecret != null)
+
+        val prefs = getPreferences(MODE_PRIVATE)
+        prefs.getString(authTokenProp, null)?.let { authToken = String(Base64.decode(it, Base64.DEFAULT)) }
+        prefs.getString(authTokenSecretProp, null)?.let { authTokenSecret = String(Base64.decode(it, Base64.DEFAULT)) }
     }
 
     fun doAuth() {
@@ -48,11 +67,24 @@ class AuthActivity: AppCompatActivity() {
     }
 
     fun onLogin(r: LoginResult) {
+        authToken = r.oAuthToken
+        authTokenSecret = r.oAuthTokenSecret
+        val editor = getPreferences(MODE_PRIVATE).edit()
+        editor.putString(authTokenProp, Base64.encodeToString(r.oAuthToken.toByteArray(Charsets.UTF_8), Base64.DEFAULT))
+        editor.putString(authTokenSecretProp, Base64.encodeToString(r.oAuthTokenSecret.toByteArray(Charsets.UTF_8), Base64.DEFAULT))
+        val committed = editor.commit()
+        assert(committed)
+        goToDashboard()
+    }
+
+    fun goToDashboard() {
+        assert(authToken != null, { "authToken がまだ null のまま Dashboard を開こうとしている" })
+        assert(authTokenSecret != null, { "authTokenSecret がまだ null のまま Dashboard を開こうとしている" })
         var intent = Intent(this, DashboardActivity::class.java)
         intent.putExtra("consumerKey", consumerKey)
         intent.putExtra("consumerSecret", consumerSecret)
-        intent.putExtra("authToken", r.oAuthToken)
-        intent.putExtra("authTokenSecret", r.oAuthTokenSecret)
+        intent.putExtra("authToken", authToken)
+        intent.putExtra("authTokenSecret", authTokenSecret)
         startActivity(intent)
     }
 
