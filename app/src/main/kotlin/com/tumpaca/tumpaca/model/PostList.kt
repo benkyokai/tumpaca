c28f8096063784de840ea14d2b07cb08c8b2d0e5
Yuichi Yabu <yuichi.yabu@ripplex.com>
2016-10-07 15:44:18 +0900

    week11-yabu 260件問題修正中

--- app/src/main/kotlin/com/tumpaca/tumpaca/model/PostList.kt
+++ app/src/main/kotlin/com/tumpaca/tumpaca/model/PostList.kt
@@ -47,6 +47,7 @@ class PostList(private val client: JumblrClient) {
     // SUPPORTED_TYPES で列挙されたタイプでフィルタリングされたポストリスト
     // バックグラウンドスレッドからもアクセスするのでスレッドセーフリストを使う必要あり。
     private val posts: CopyOnWriteArrayList<Post> = CopyOnWriteArrayList()
+    private val tmpPosts: CopyOnWriteArrayList<Post> = CopyOnWriteArrayList()
 
     // fetch はいまのところ UI スレッドからのみアクセスするので volatile いらない
     private var fetching: Boolean = false
@@ -116,18 +117,31 @@ class PostList(private val client: JumblrClient) {
         return remain < FETCH_MIN_POST_NUM // 最小よりも小さかったら fetch が必要
     }
 
-    private fun fetch(count: Int) {
-        fetchImpl(count)
+    /**
+     * fetchSizeは目安です。fetchSize以上の件数を取得することもあります。
+     */
+    private fun fetch(fetchSize: Int) {
+        fetchImpl(fetchSize)
     }
 
     private fun fetchImpl(fetchSize: Int) {
         fetching = true
 
         if (fetchSize <= 0) {
+            tmpPosts.removeAll(tmpPosts)
             fetching = false
             return
         }
 
+        //fetchByOffset(fetchSize)
+        if (offset <= 20) {
+            fetchByOffset(fetchSize)
+        } else {
+            fetchBySinceId(fetchSize)
+        }
+    }
+
+    private fun fetchByOffset(fetchSize: Int) {
         // TODO: PostListの参照が漏れないようにこのタスクを独立クラス化する
         object : AsyncTask<Void, Void, List<Post>>() {
             override fun doInBackground(vararg args: Void): List<Post> {
@@ -156,12 +170,6 @@ class PostList(private val client: JumblrClient) {
                     SUPPORTED_TYPES.contains(it.type)
                 }
 
-                if (result.size != filteredResult.size) {
-                    // TODO fetch の結果 filter されると、現状の post サイズとインターネット上の post のインデックスが
-                    // 合わなくなるのでフィルタリングは外側でやったほうがいい
-                    Log.w(TAG, "Some posts are filtered: ${result.size}=>${filteredResult.size}");
-                }
-
                 posts.addAll(filteredResult)
                 Log.v(TAG, "Loaded ${result.size} posts, size=$size")
                 listeners.forEach { it.onChanged() }
@@ -171,6 +179,134 @@ class PostList(private val client: JumblrClient) {
                 } // 取得したポストの数が0なら次回のロードはしない
             }
         }.execute()
+    }
+
+    private fun fetchBySinceId(fetchSize: Int) {
+        val sinceId = nextId()
+        Log.v(TAG, "sinceId fetching")
+
+        object : AsyncTask<Void, Void, List<Post>>() {
+            override fun doInBackground(vararg args: Void): List<Post> {
+                // ここはバックグラウンドスレッド
+                val parameter = hashMapOf(
+                        "since_id" to sinceId,
+                        "limit" to FETCH_LIMIT,
+                        "reblog_info" to true,
+                        "notes_info" to true)
+                Log.v(TAG, "try to load since_id -> $sinceId")
+                try {
+                    return client.userDashboard(parameter)
+                } catch (e: Throwable) {
+                    // TODO エラー処理
+                    Log.e(TAG, "PostList fetch error: ${e.message}")
+                    fetching = false
+                    return emptyList()
+                }
+            }
+
+            override fun onPostExecute(result: List<Post>) {
+                // ここは UI スレッド
+                val filteredResult = result.filter {
+                    SUPPORTED_TYPES.contains(it.type)
+                }
+
+                val r = checkResult(filteredResult)
+
+                when (r) {
+                    Result.EMPTY -> {
+                        Log.v(TAG, "EMPTY")
+                        tmpPosts.removeAll(tmpPosts)
+                    }
+                    Result.FAR_AWAY -> {
+                        tmpPosts.addAll(0, filteredResult)
+                        Log.v(TAG, "FAR_AWAY tmpPosts=${tmpPosts.size}")
+                    }
+                    Result.DUPLICATE -> {
+                        Log.v(TAG, "DUPLICATE")
+                    }
+                    Result.GOOD -> {
+                        val i = filteredResult.indexOf(posts.last())
+                        tmpPosts.addAll(0, filteredResult.drop(i + 1))
+                        Log.v(TAG, "GOOD tmpPosts=${tmpPosts.size}")
+                    }
+                }
+
+                Log.v(TAG, "Loaded ${result.size} posts, size=$size")
+                //listeners.forEach { it.onChanged() }
+                fetching = false
+                if (r == Result.GOOD || (r == Result.DUPLICATE && tmpPosts.isNotEmpty()) || (r == Result.EMPTY && tmpPosts.isNotEmpty())) {
+                    rate = 1.0
+                    posts.addAll(tmpPosts)
+                    val tmpPostsCount = tmpPosts.size
+                    tmpPosts.removeAll(tmpPosts)
+                    listeners.forEach { it.onChanged() }
+                    fetchImpl(fetchSize - tmpPostsCount)
+                } else if (r == Result.DUPLICATE && tmpPosts.isEmpty()) {
+                    rate = rate + 1.0
+                    Log.v(TAG, "DUPLICATE and tmpPosts is empty, rate = ${rate}")
+                    fetchImpl(fetchSize)
+                } else if (r == Result.EMPTY && tmpPosts.isEmpty()) {
+                    rate = 1.0
+                    return
+                } else if (r == Result.FAR_AWAY) {
+                    if (fetchSize - tmpPosts.size < 0) {
+                        tmpPosts.removeAll(tmpPosts)
+
+                        if (rate > 1.0) {
+                            rate = rate - 0.7
+                        } else {
+                            rate = rate * 0.5
+                        }
+                        Log.v(TAG, "FAR_AWAY fetchSize < 0, rate = ${rate}")
+                        fetchImpl(fetchSize)
+                    } else {
+                        rate = 1.0
+                        fetchImpl(fetchSize - tmpPosts.size)
+                    }
+                } else {
+                    Log.v(TAG, "ここには来ないはず")
+                    throw RuntimeException("PostList: Result is invalid. ${r}, tmpPosts.size == ${tmpPosts.size}")
+                }
+            }
+        }.execute()
+    }
+
+    private var rate = 1.0
+
+    private fun nextId(): Long {
+        if (tmpPosts.size > 0) {
+            return tmpPosts.first().id
+        }
+
+        if (posts.size <= 0) {
+            return 0
+        }
+
+        val diff = ((posts.first().id - posts.last().id) / posts.size) * FETCH_LIMIT
+        Log.v(TAG, "diff: ${diff}")
+
+        val sinceId = posts.last().id - (diff * rate).toLong()
+
+        return if (sinceId <= 0) 0 else sinceId
+    }
+
+    enum class Result {
+        GOOD, FAR_AWAY, DUPLICATE, EMPTY
+    }
+
+    private fun checkResult(result: List<Post>): Result {
+        if (result.isEmpty()) {
+            return Result.EMPTY
+        }
+
+        val firstPost = result[0]
+        if (posts.last().id > firstPost.id) {
+            return Result.FAR_AWAY
+        } else if (posts.last().id >= result.last().id) {
+            return Result.DUPLICATE
+        } else {
+            return Result.GOOD
+        }
     }
 
     private fun refreshUser() {
