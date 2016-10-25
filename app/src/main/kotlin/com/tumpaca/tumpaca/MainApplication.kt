0ff4ad58514d390f2d4448d07cd8e66e3fbf9bda
Hajime Shiozawa <shiozawa@ripplex.com>
2016-10-25 15:52:44 +0900

    issues/#24 Firebase analytics の初期化

--- app/src/main/kotlin/com/tumpaca/tumpaca/MainApplication.kt
+++ app/src/main/kotlin/com/tumpaca/tumpaca/MainApplication.kt
@@ -2,6 +2,7 @@ package com.tumpaca.tumpaca
 
 import android.app.Application
 import android.util.Log
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.tumpaca.tumpaca.model.TPRuntime
 
 class MainApplication : Application() {
@@ -9,9 +10,16 @@ class MainApplication : Application() {
         private const val TAG = "MainApplication"
     }
 
+    lateinit var analytics: FirebaseAnalytics
+
     override fun onCreate() {
         super.onCreate()
         Log.d(TAG, "onCreate()")
         TPRuntime.initialize(this)
+
+        analytics = FirebaseAnalytics.getInstance(this)
+        if (BuildConfig.FIREBASE_ENABLED) {
+            analytics.setAnalyticsCollectionEnabled(true)
+        }
     }
 }
