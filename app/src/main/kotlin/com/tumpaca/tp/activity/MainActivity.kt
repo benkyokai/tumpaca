5a729d032c1e62a31a079c84422d0126ea91e71d
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-02-08 23:32:20 +0900

    AdMob App IDをプロパティファイルから読み込む

--- app/src/main/kotlin/com/tumpaca/tp/activity/MainActivity.kt
+++ app/src/main/kotlin/com/tumpaca/tp/activity/MainActivity.kt
@@ -2,11 +2,13 @@ package com.tumpaca.tp.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Base64
 import android.util.Log
 import com.google.android.gms.ads.MobileAds
 import com.tumpaca.tp.R
 import com.tumpaca.tp.fragment.AuthFragment
 import com.tumpaca.tp.model.TPRuntime
+import java.util.Properties
 
 /**
  * Tumpaca クラスのメインアクティビティ。
@@ -14,6 +16,7 @@ import com.tumpaca.tp.model.TPRuntime
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "MainActivity"
+        private const val ADMOB_APP_ID_PROP = "admob.app.id"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
         TPRuntime.mainApplication.analytics.logEvent("launched", null)
 
-        MobileAds.initialize(applicationContext, "ca-app-pub-3940256099942544~3347511713");
+        MobileAds.initialize(applicationContext, loadAdMobAppId());
 
         if (savedInstanceState == null) {
             val start = AuthFragment()
@@ -33,5 +36,13 @@ class MainActivity : AppCompatActivity() {
             ft.replace(R.id.fragment_container, start)
             ft.commit()
         }
+    }
+
+    private fun loadAdMobAppId(): String {
+        val admobProps = Properties()
+        resources.openRawResource(R.raw.admob).use { admobProps.load(it) }
+        val id = String(Base64.decode(admobProps[ADMOB_APP_ID_PROP] as String, Base64.DEFAULT))
+        Log.d(TAG, "Loaded AdMob app ID: id=$id")
+        return id
     }
 }
