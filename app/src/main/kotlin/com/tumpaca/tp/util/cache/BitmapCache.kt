2186bb31289bd7e31a6f5ed4f3c8545fe915b73c
Hajime Shiozawa <shiozawa@ripplex.com>
2017-09-04 14:27:58 +0900

    refactoring cache

--- app/src/main/kotlin/com/tumpaca/tp/util/cache/BitmapCache.kt
+++ app/src/main/kotlin/com/tumpaca/tp/util/cache/BitmapCache.kt
@@ -13,85 +13,54 @@ interface Cache<T> {
     fun getIfNoneAndSet(key: String, f: () -> T?): T?
 }
 
-// TODO Cache の統一化
-class BitmapCache : Cache<Bitmap> {
-    companion object {
-        val TAG = "BitmapCache"
+abstract class AbstractCache<T> : Cache<T> {
+    abstract fun getTag(): String
+    abstract fun getLruCache(): LruCache<String, T>
+
+    override fun set(key: String, value: T) {
+        getLruCache().put(key, value)
     }
 
-    private val MAX_SIZE = 32 * 1024 * 1024
-
-    private val lruCache = object : LruCache<String, Bitmap>(MAX_SIZE) {
-        override fun sizeOf(key: String, value: Bitmap): Int {
-            return value.allocationByteCount
-        }
+    override fun get(key: String): T? {
+        return getLruCache().get(key)
     }
 
-    override fun set(key: String, value: Bitmap) {
-        lruCache.put(key, value)
-    }
-
-    override fun get(key: String): Bitmap? {
-        return lruCache.get(key)
-    }
-
-    override fun getIfNoneAndSet(key: String, f: () -> Bitmap?): Bitmap? {
+    override fun getIfNoneAndSet(key: String, f: () -> T?): T? {
         get(key)?.let {
-            Log.d(BitmapCache.TAG, "cache hit: key=$key, cache=$lruCache, evict=${lruCache.evictionCount()}")
+            Log.d(getTag(), "cache hit: key=$key, cache=${getLruCache()}, evict=${getLruCache().evictionCount()}")
             return it
         }
 
         try {
             f()?.let {
-                Log.d(BitmapCache.TAG, "cache not hit and set: key=$key, cache=$lruCache, evict=${lruCache.evictionCount()}")
+                Log.d(getTag(), "cache not hit and set: key=$key, cache=${getLruCache()}, evict=${getLruCache().evictionCount()}")
                 set(key, it)
                 return it
             }
         } catch (e: Throwable) {
-            Log.e(BitmapCache.TAG, "BitmapCache fetch error: ${e.message}", e)
+            Log.e(getTag(), "BitmapCache fetch error: ${e.message}", e)
         }
 
         return null
     }
 }
 
-class AvatarUrlCache : Cache<String> {
-    companion object {
-        val TAG = "AvatarUrlCache"
+class BitmapCache : AbstractCache<Bitmap>() {
+    private val MAX_SIZE = 32 * 1024 * 1024
+    private val lruCache = object : LruCache<String, Bitmap>(MAX_SIZE) {
+        override fun sizeOf(key: String, value: Bitmap): Int = value.allocationByteCount
     }
 
+    override fun getTag(): String = "BitmapCache"
+    override fun getLruCache(): LruCache<String, Bitmap> = lruCache
+}
+
+class AvatarUrlCache : AbstractCache<String>() {
     private val MAX_SIZE = 1 * 1024 * 1024
-
     private val lruCache = object : LruCache<String, String>(MAX_SIZE) {
-        override fun sizeOf(key: String, value: String): Int {
-            return value.count()
-        }
+        override fun sizeOf(key: String, value: String): Int = value.count()
     }
 
-    override fun set(key: String, value: String) {
-        lruCache.put(key, value)
-    }
-
-    override fun get(key: String): String? {
-        return lruCache.get(key)
-    }
-
-    override fun getIfNoneAndSet(key: String, f: () -> String?): String? {
-        get(key)?.let {
-            Log.d(TAG, "cache hit: key=$key, cache=$lruCache, evict=${lruCache.evictionCount()}")
-            return it
-        }
-
-        try {
-            f()?.let {
-                Log.d(TAG, "cache not hit and set: key=$key, cache=$lruCache, evict=${lruCache.evictionCount()}")
-                set(key, it)
-                return it
-            }
-        } catch (e: Throwable) {
-            Log.e(TAG, "AvatarUrlCache fetch error: ${e.message}", e)
-        }
-
-        return null
-    }
+    override fun getTag(): String = "AvatarUrlCache"
+    override fun getLruCache(): LruCache<String, String> = lruCache
 }
