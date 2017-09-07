d60e1c117ff7067b8a669477eeaafb1308153284
Hajime Shiozawa <shiozawa@ripplex.com>
2017-09-07 17:11:13 +0900

    抽象プロパティを利用

--- app/src/main/kotlin/com/tumpaca/tp/util/cache/Cache.kt
+++ app/src/main/kotlin/com/tumpaca/tp/util/cache/Cache.kt
@@ -14,29 +14,24 @@ interface Cache<T> {
 }
 
 abstract class AbstractCache<T> : Cache<T> {
-    abstract fun getTag(): String
-    abstract fun getLruCache(): LruCache<String, T>
+    abstract protected val tag: String
+    abstract protected val lruCache: LruCache<String, T>
 
     override fun set(key: String, value: T) {
-        getLruCache().put(key, value)
+        lruCache.put(key, value)
     }
 
-    override fun get(key: String): T? {
-        return getLruCache().get(key)
-    }
+    override fun get(key: String): T? = lruCache.get(key)
 
     override fun getIfNoneAndSet(key: String, f: () -> T?): T? {
-        val tag = getTag()
-        val cache = getLruCache()
-
         get(key)?.let {
-            Log.d(tag, "cache hit: key=$key, cache=$cache, evict=${cache.evictionCount()}")
+            Log.d(tag, "cache hit: key=$key, cache=$lruCache, evict=${lruCache.evictionCount()}")
             return it
         }
 
         try {
             f()?.let {
-                Log.d(tag, "cache not hit and set: key=$key, cache=$cache, evict=${cache.evictionCount()}")
+                Log.d(tag, "cache not hit and set: key=$key, cache=$lruCache, evict=${lruCache.evictionCount()}")
                 set(key, it)
                 return it
             }
@@ -51,32 +46,26 @@ abstract class AbstractCache<T> : Cache<T> {
 class BitmapCache : AbstractCache<Bitmap>() {
     // 単位はバイト
     private val MAX_SIZE = 32 * 1024 * 1024
-    private val lruCache = object : LruCache<String, Bitmap>(MAX_SIZE) {
+    override val lruCache = object : LruCache<String, Bitmap>(MAX_SIZE) {
         override fun sizeOf(key: String, value: Bitmap): Int = value.allocationByteCount
     }
-
-    override fun getTag(): String = "BitmapCache"
-    override fun getLruCache(): LruCache<String, Bitmap> = lruCache
+    override val tag = "BitmapCache"
 }
 
 class AvatarUrlCache : AbstractCache<String>() {
     // 単位は文字数。
     // 1URL = 100 文字として、100 アバター保存できれば十分と考える
     private val MAX_CHAR_COUNT = 100 * 100
-    private val lruCache = object : LruCache<String, String>(MAX_CHAR_COUNT) {
+    override val lruCache = object : LruCache<String, String>(MAX_CHAR_COUNT) {
         override fun sizeOf(key: String, value: String): Int = value.count()
     }
-
-    override fun getTag(): String = "AvatarUrlCache"
-    override fun getLruCache(): LruCache<String, String> = lruCache
+    override val tag = "AvatarUrlCache"
 }
 
 class GifCache : AbstractCache<ByteArray>() {
     private val MAX_SIZE = 32 * 1024 * 1024
-    private val lruCache = object : LruCache<String, ByteArray>(MAX_SIZE) {
+    override val lruCache = object : LruCache<String, ByteArray>(MAX_SIZE) {
         override fun sizeOf(key: String, value: ByteArray): Int = value.size
     }
-
-    override fun getTag(): String = "GifCache"
-    override fun getLruCache(): LruCache<String, ByteArray> = lruCache
+    override val tag = "GifCache"
 }
